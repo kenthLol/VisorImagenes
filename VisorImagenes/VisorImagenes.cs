7d130630242a88b0157ce592c31679d9f551e46c
wegner01 <kenneth.lola119@est.unanleon.edu.ni>
2023-04-25 23:06:41 -0600

    feat: Added new commands for File, Window, and Help

--- VisorImagenes/VisorImagenes.cs
+++ VisorImagenes/VisorImagenes.cs
@@ -16,5 +16,31 @@ namespace VisorImagenes
         {
             InitializeComponent();
         }
+
+        private void ayudaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox1 aboutBox1 = new AboutBox1();
+            aboutBox1.ShowDialog();
+        }
+
+        private void cascadaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void mosaicoHorizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void mosaicoVerticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
