c2979f8ad7621f2acb84c331faf1d9614570d1f0
Jeremy Fonseca <fonseca.dev@outlook.com>
2023-04-28 15:40:07 -0700

    Gray scale, add more than one image and save them.

--- VisorImagenes/VisorImagenes.cs
+++ VisorImagenes/VisorImagenes.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Windows.Forms;
 
 namespace VisorImagenes
@@ -19,7 +14,7 @@ namespace VisorImagenes
 
         public VentanaHija HijaActiva
         {
-            get { return (VentanaHija)this.ActiveMdiChild; }
+            get { return this.ActiveMdiChild as VentanaHija; }
         }
 
         private void ayudaToolStripMenuItem_Click(object sender, EventArgs e)
@@ -50,17 +45,23 @@ namespace VisorImagenes
 
         private void nuevoToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            string título = "Doc" + (this.MdiChildren.Length + 1);
-            NuevaHija(título);
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.jpg;*.jpeg;*.png;*.gif)|*.jpg;*.jpeg;*.png;*.gif";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string title = "Doc" + (this.MdiChildren.Length + 1);
+                NuevaHija(title, System.Drawing.Image.FromFile(openFileDialog.FileName));
+            }
         }
 
-        private void NuevaHija(string título)
+        private void NuevaHija(string título, System.Drawing.Image imagen = null)
         {
-            VentanaHija hija = new VentanaHija(título);
-            hija.MdiParent = this;
-            hija.PictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
-            hija.AutoScroll = true;
-            hija.Show();
+            VentanaHija fh = new VentanaHija(título);
+            fh.Text = título;
+            fh.MdiParent = this;
+            fh.PictureBox.Image = imagen;
+            fh.PictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            fh.Show();
         }
 
         private void cerrarToolStripMenuItem_Click(object sender, EventArgs e)
@@ -76,6 +77,52 @@ namespace VisorImagenes
         private void escribirTextoToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.HijaActiva.Escribir();
+        }
+
+        private void escalaDeGrisesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.HijaActiva.ConvertirEscalaDeGrises();
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Imagen JPEG|*.jpg|Imagen PNG|*.png|Imagen BMP|*.bmp|Todos los archivos|*.*";
+            saveFileDialog.Title = "Guardar imagen como...";
+            saveFileDialog.FileName = "Sin título";
+
+            saveFileDialog.ShowDialog();
+
+            if (saveFileDialog.FileName != "")
+            {
+                string fileName = saveFileDialog.FileName;
+                string fileExtension = Path.GetExtension(fileName).ToLower();
+
+                // Guardar la imagen con el encoding apropiado.
+                ImageFormat imageFormat;
+                switch (fileExtension)
+                {
+                    case ".jpg":
+                        imageFormat = ImageFormat.Jpeg;
+                        break;
+                    case ".png":
+                        imageFormat = ImageFormat.Png;
+                        break;
+                    case ".bmp":
+                        imageFormat = ImageFormat.Bmp;
+                        break;
+                    default:
+                        imageFormat = ImageFormat.Jpeg;
+                        break;
+                }
+
+                this.HijaActiva.PictureBox.Image.Save(fileName, imageFormat);
+            }
+        }
+
+        private void VisorImagenes_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
         }
     }
 }
